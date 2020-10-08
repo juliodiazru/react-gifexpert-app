727e1dae06add81b579f4c0c846d54db37326bb2
Julio Diaz <juliodiazru@gmail.com>
2020-10-08 15:20:33 -0500

    docs agregado

---
+++ docs/precache-manifest.e7f10300e8526aa88b42b4f2f20d4609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e10a8a65cfe25b7853634ec52b7bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5590eba3db40d9cdb477",
+    "url": "/static/css/main.9545a183.chunk.css"
+  },
+  {
+    "revision": "1f1c1ba68c2b85f8fe4e",
+    "url": "/static/js/2.069e8248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.069e8248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5590eba3db40d9cdb477",
+    "url": "/static/js/main.2d4b78a1.chunk.js"
+  },
+  {
+    "revision": "21c9ab102272ccd620a7",
+    "url": "/static/js/runtime-main.cc15516c.js"
+  }
+]);
